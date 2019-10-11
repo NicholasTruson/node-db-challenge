67caef51ce60d2284e3dca0dafb28c170e232d6f
Nicholas Truson <nicholas.truson@gmail.com>
2019-10-11 10:34:30 -0700

    resource router

--- data/seeds/001-projects.js
+++ data/seeds/001-projects.js
@@ -1,17 +1,17 @@
 exports.seed = function(knex, Promise) {
   return knex("projects").insert([
     {
-      project_name: "Finish This Sprint",
+      project_name: "Finish This Sprint", // 1
       project_description: "must get to the MVP",
       completed: 0
     },
     {
-      project_name: "Graduate From Lambda",
+      project_name: "Graduate From Lambda", // 2
       project_description: "Must get to the Graduation",
       completed: 0
     },
     {
-      project_name: "Land a badass Job",
+      project_name: "Land a badass Job", // 3
       project_description: "Must make the money",
       completed: 0
     }
