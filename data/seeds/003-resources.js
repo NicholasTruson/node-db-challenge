09fab1584f47683faa8e10bf91570e362a0a9c8a
Nicholas Truson <nicholas.truson@gmail.com>
2019-10-11 09:54:15 -0700

    resource seed data

--- data/seeds/003-resources.js
+++ data/seeds/003-resources.js
@@ -1,13 +1,28 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+  return knex("resources").insert([
+    {
+      resource_name: "resource 1",
+      resource_description: "this is useful"
+    },
+    {
+      resource_name: "resource 2",
+      resource_description: "this is useful"
+    },
+    {
+      resource_name: "resource 3",
+      resource_description: "this is useful"
+    },
+    {
+      resource_name: "resource 4",
+      resource_description: "this is useful"
+    },
+    {
+      resource_name: "resource 5",
+      resource_description: "this is useful"
+    },
+    {
+      resource_name: "resource 6",
+      resource_description: "this is useful"
+    }
+  ]);
+}
