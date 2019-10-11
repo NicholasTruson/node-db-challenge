187e5b0a272ce36dcb3f85d9c7a3be81739b20ad
Nicholas Truson <nicholas.truson@gmail.com>
2019-10-11 09:05:24 -0700

    task and resource schema

--- migrations/20191011083032_bootstrap.js
+++ migrations/20191011083032_bootstrap.js
@@ -1,5 +1,12 @@
 exports.up = function(knex) {
     return knex.schema
+/* PROJECT
+a unique Id.
+ a name. This column is required.
+ a description.
+ a boolean that indicates if the project has been completed. This column cannot be NULL, the default value should be false.
+ */
+
       .createTable("projects", tbl => {
         tbl.increments();
 
@@ -12,7 +19,49 @@ exports.up = function(knex) {
           .defaultTo(0); // returns a default false
       })
 
+/* TASK
+ a unique id.
+ a description of what needs to be done. This column is required.
+ a notes column to add additional information.
+ a boolean that indicates if the task has been completed. This column cannot be NULL, the default value should be false.
+*/
+
+.createTable("tasks", tbl => {
+    tbl.increments();
+
+    tbl.string("task_description").notNullable();
+    tbl.string("task_notes");
+    tbl
+      .boolean("completed")
+      .notNullable()
+      .defaultTo(0); // FALSE DEFAULT
+    tbl
+      .integer("project_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("projects")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  })
+
+/* RESOURCE
+ a unique Id.
+ a name. This column is required.
+ a description.
+*/
+
+.createTable("resources", tbl => {
+    tbl.increments();
+
+    tbl
+      .string("resource_name")
+      .notNullable()
+      .unique();
+    tbl.string("resource_description");
+  })
+
 exports.down = function(knex) {
     return knex.schema
 
-}
+}}
