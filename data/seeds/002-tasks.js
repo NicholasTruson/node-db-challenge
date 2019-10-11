33a7ed775a15eb58a0424a8ad7321c2c9746ecd1
Nicholas Truson <nicholas.truson@gmail.com>
2019-10-11 09:51:06 -0700

    task seed data

--- data/seeds/002-tasks.js
+++ data/seeds/002-tasks.js
@@ -1,13 +1,40 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+  return knex("tasks").insert([
+    {
+      task_description: "Commit Frequently",
+      task_notes: "document the process",
+      completed: 0,
+      project_id: 1
+    },
+    {
+      task_description: "Remember the ReadMe Questions",
+      task_notes: "you always forget",
+      completed: 0,
+      project_id: 1
+    },
+    {
+      task_description: "Catch Up on Career Assignments",
+      task_notes: "It is important",
+      completed: 0,
+      project_id: 2
+    },
+    {
+      task_description: "Polish Up Old Projects",
+      task_notes: "They are a mess",
+      completed: 0,
+      project_id: 2
+    },
+    {
+      task_description: "Reach out to employers",
+      task_notes: "get used to it",
+      completed: 0,
+      project_id: 3
+    },
+    {
+      task_description: "Practice Interviewing",
+      task_notes: "become a smooth talker",
+      completed: 0,
+      project_id: 3
+    },
+  ]);
+}
